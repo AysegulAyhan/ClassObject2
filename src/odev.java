bebbfbb54307908bd2dffbf2e9f18a96a219057b
AYSEGUL <aysegulayhan93@icloud.com>
2020-05-14 00:57:11 -0400

    Java Course Codes

---
+++ src/odev.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class odev {
+
+   // Create a 2D String Array which is
+//[{"new jersey","atlanta","ohio"} ,
+    //{"Pittsburgh" ,"ohio","new york","ohio"} ,
+   // {"ohio","new york"}]
+
+   // Change all the ohio to Florida
+
+   // print the array
+   public static void main(String[] args) {
+       String[][] citys={{"new jersey","atlanta","ohio"} ,
+       {"Pittsburgh" ,"ohio","new york","ohio"} ,
+        {"ohio","new york"}};
+
+       for (int i = 0; i <citys.length ; i++) {
+           for (int j = 0; j <citys[i].length ; j++) {
+               if(citys[i][j].contains("ohio")){
+                  citys[i][j]="florida";
+               }
+               System.out.println(citys[i][j]+" ");
+           }
+
+       }
+
+   }
+
+}
